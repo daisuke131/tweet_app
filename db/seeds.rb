16be43a4a2f80f0d79e18e360ec89001269b4b04
daisuke <kurimotodaisuke@kurimotodaisukenoMacBook-Pro.local>
2019-02-16 22:47:21 +0900

    学習Ⅱまで

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+    Post.create(content: "今日からProgateでRailsの勉強するよー！")
+    Post.create(content: "投稿一覧ページ作成中！")
