2c46bee0edf0f3ff47118af4fd4b2653b6f76c16
daisuke <kurimotodaisuke@kurimotodaisukenoMacBook-Pro.local>
2019-02-17 21:49:07 +0900

    Ⅳまで終了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    @id = params[:id]
+    # @id = params[:id]
     @post = Post.find_by(id: params[:id])
   end
 
@@ -15,6 +15,23 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(content: params[:content])
     @post.save
+    redirect_to("/posts/index")
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    redirect_to("/posts/index")
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
     redirect_to("/posts/index")
   end
 end
