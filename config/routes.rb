cbc8618d64ae4150e204a39022b699479d9451bb
daisuke <kurimotodaisuke@kurimotodaisukenoMacBook-Pro.local>
2019-02-18 23:18:22 +0900

    Ⅵまで終了　ユーザー管理

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'signup' => "users#new"
+  post 'users/create' => "users#create"
+  get 'users/:id' => "users#show"
+  get 'users/:id/edit' => "users#edit"
+  post 'users/:id/update' => "users#update"
+  post 'users/:id/destroy' => "users#destroy"
+
   get 'posts/index' => "posts#index"
   get 'posts/new' => "posts#new"
   get 'posts/:id' => "posts#show"
