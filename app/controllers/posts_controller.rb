d400913e39eaba4aee6bfdbb297e00ac1798145d
daisuke <kurimotodaisuke@kurimotodaisukenoMacBook-Pro.local>
2019-02-24 00:00:03 +0900

    Ⅸまで終了　ログインユーザー以外編集できないように

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,15 @@
 class PostsController < ApplicationController
 
   before_action :authenticate_user
+  before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
+
   def index
     @posts = Post.all.order(created_at: :desc)
   end
 
   def show
     @post = Post.find_by(id: params[:id])
+    @user = User.find_by(id: @post.user_id)
   end
 
   def new
@@ -14,13 +17,15 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(content: params[:content])
+    @post = Post.new(
+      content: params[:content], 
+      user_id: @current_user.id)
     if @post.save
       flash[:notice] = "投稿完了！"
-    redirect_to("/posts/index")
-  else
-    render("posts/new")
-  end
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
   end
 
   def edit
@@ -32,7 +37,7 @@ class PostsController < ApplicationController
     @post.content = params[:content]
     if @post.save
       flash[:notice] = "投稿を編集しました"
-    redirect_to("/posts/index")
+      redirect_to("/posts/index")
     else
       render("posts/edit")
     end
@@ -44,4 +49,13 @@ class PostsController < ApplicationController
     flash[:notice] = "投稿を削除しました"
     redirect_to("/posts/index")
   end
+
+  def ensure_correct_user
+    @post = Post.find_by(id: params[:id])
+    if @current_user.id != @post.user_id
+      flash[:notice] = '権限がありません'
+      redirect_to('/posts/index')
+    end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user, only: %i[index show edit update]
-  before_action :forbid_login_user, only: %i[new create login_form login]
+  before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
+  before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
   before_action :ensure_correct_user, {only: [:edit, :update]}
 
   def index
