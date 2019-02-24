be355b1a9e730cd49c3ad20d9e62d476d0f9a201
daisuke <kurimotodaisuke@kurimotodaisukenoMacBook-Pro.local>
2019-02-24 12:18:33 +0900

    Ⅹ終了　いいね機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  post 'likes/:post_id/create' => 'likes#create'
+  post 'likes/:post_id/destroy' => 'likes#destroy'
+
   get 'login' => 'users#login_form'
   post 'login' => 'users#login'
   get 'users/index' => "users#index"
@@ -10,6 +13,7 @@ Rails.application.routes.draw do
   get 'users/:id/edit' => "users#edit"
   post 'users/:id/update' => "users#update"
   post 'users/:id/destroy' => "users#destroy"
+  get 'users/:id/likes' => "users#likes"
 
   get 'posts/index' => "posts#index"
   get 'posts/new' => "posts#new"
