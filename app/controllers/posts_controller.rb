58a8c4286e8c2f065ed62653baefd84183d91099
daisuke <kurimotodaisuke@kurimotodaisukenoMacBook-Pro.local>
2019-02-24 23:52:25 +0900

    いいね一覧の表示バグ修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -46,6 +46,10 @@ class PostsController < ApplicationController
 
   def destroy
     @post = Post.find_by(id: params[:id])
+
+    @likes = Like.where(post_id: @post.id)
+    @likes.destroy_all
+    
     @post.destroy
     flash[:notice] = "投稿を削除しました"
     redirect_to("/posts/index")
